96e7abddc018515b79235caa5a33b7c71119e5f1
Damian Hofmann <2hofmann@informatik.uni-hamburg.de>
2014-05-20 16:32:58 +0200

    Header zu essig.h zusammengefuehrt

--- code/essig.h
+++ code/essig.h
@@ -1,9 +1,51 @@
-#ifndef ESSIG_SIMULATION_H
-#define ESSIG_SIMULATION_H
 
-#include <stdbool.h>
 
-#include "data-structures.h"
+#ifndef ESSIG_DATASTRUCTURES_H
+#define ESSIG_DATASTRUCTURES_H
+
+#include <stdio.h>
+
+typedef double[3] Vector;
+typedef double[3][3] Matrix;
+
+typedef struct
+{
+  int    id;
+  Vector pos;
+  char  *element_symbol;
+} Atom;
+
+/**
+ * Represents a chemical bond around which the molecule may be rotated.
+ */
+typedef struct
+{
+  /// Pointer to the first bound atom.
+  Atom *first;
+  /// Pointer to the second bound atom.
+  Atom *second;
+  /// Array of pointers to the atom behind 'first'.
+  /// Stored here as an optimization.
+  Atom **left;
+  size_t left_count;
+  /// Array of pointers to the atom behind 'second'.
+  /// Stored here as an optimization.
+  Atom **right;
+  size_t right_count;
+} Bond;
+
+typedef struct
+{
+  /// Array of the atoms forming the molecule.
+  Atom *atoms;
+  /// Length of 'atoms'.
+  size_t atom_count;
+  /// Array of the rotatable chemical bonds of the molecule.
+  Bond *bonds;
+  /// Length of 'bonds'.
+  size_t bond_count;
+  int charge;
+} Molecule;
 
 /**
  * Simulates the molecule using orca and outputs the calculated
@@ -32,5 +74,22 @@ void run_simulation(Molecule *mol,
                     double rotation_translation_ratio,
                     double temperature,
                     bool output_intermediate);
+
+/**
+ * Loads molecule data from the file 'fp',
+ * TODO: Currently only returns the data of a hard-coded
+ *       CH3COOH molecule.
+ */
+Molecule *molecule_new_from_file(FILE *fp);
+
+/**
+ * Frees the memory allocated for the molecule data.
+ */
+void molecule_free(Molecule *mol);
+
+/**
+ * Run Orca to get the energy of the given molecule.
+ */
+double get_energy(Molecule *mol);
 
 #endif
