e1e6dd2789e030bd533f43acf947676408b7ea00
Sebastian <3fett@inf>
2014-05-26 12:41:44 +0200

    orca_calculate_energy von mock.c durch fertige Implementation ersetzt, dabei Makefile etwas aufgeräumt

--- code/mock.c
+++ code/mock.c
@@ -5,30 +5,31 @@
  * until their real implementations are finished.
  *
  * Authors:
- * 	Damian Hofmann <2hofmann@inf>
+ *  Damian Hofmann <2hofmann@inf>
  */
 #include "essig.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-double orca_calculate_energy(const Molecule *mol)
-{
-  return 250 + 5*drand48();
-}
-
 void molden_output_molecule(const Molecule *mol)
 {
+    (void) mol;
 }
 
 void transform_reset_origin(Molecule *mol)
 {
+    (void) mol;
 }
 
 void transform_random_displacement(Molecule *mol, double max_dist)
 {
+    (void) mol;
+    (void) max_dist;
 }
 
 void transform_random_rotation(Molecule *mol, double max_angle)
 {
+    (void) mol;
+    (void) max_angle;
 }
