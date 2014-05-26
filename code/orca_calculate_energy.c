183a5269fc473e785470c47a7125342cb19bcd10
Damian Hofmann <2hofmann@informatik.uni-hamburg.de>
2014-05-27 00:41:46 +0200

    wip: Call orca via 'execl' instead of 'system'

--- code/orca_calculate_energy.c
+++ code/orca_calculate_energy.c
@@ -3,12 +3,17 @@
  *
  * Authors:
  *   Sebastian Fett <3fett@inf>
+ *   Damian Hofmann <2hofmann@inf>
  */
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <float.h>
 #include "essig.h"
+
+#include <errno.h>
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "vector.h"
 
 #define FILE_BASENAME "orca-data"
@@ -28,9 +33,8 @@ double orca_calculate_energy(const Molecule *mol)
 {
     FILE *file;
     unsigned long atom_number;
-    int orca_return;
-    char energy_string[60];
-    double energy = 0.0;
+    int pipefd[2];
+    pid_t pid = 0;
 
     file = fopen(FILE_BASENAME ".in", "w+");
     check_fopen(file, FILE_BASENAME ".in");
@@ -44,29 +48,76 @@ double orca_calculate_energy(const Molecule *mol)
     fprintf(file, "*\n");
     fclose(file);
 
-    orca_return = system(ORCA_PATH " " FILE_BASENAME ".in"
-        " | grep \"FINAL SINGLE POINT ENERGY\" > " FILE_BASENAME ".out");
-    if (orca_return != 0)
+    pipe(pipefd);
+    pid = fork();
+    if(pid == -1) // fork call failed
     {
-        fprintf(stderr, "Error: Orca call or Orca calculation unsuccessful!\n");
-        exit(EXIT_FAILURE);
+        perror("Error while calling orca");
     }
+    else if(pid == 0) // child process
+    {
+        close(pipefd[0]);
+        // redirect orca output to pipe
+        dup2(pipefd[1], STDOUT_FILENO);
+        // call orca
+        if(execl(ORCA_PATH, ORCA_PATH, FILE_BASENAME ".in", (char *) NULL) == -1)
+        {
+            perror("Error while calling orca");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else // parent process
+    {
+        close(pipefd[1]);
+        file = fdopen(pipefd[0], "r");
 
-    file = fopen(FILE_BASENAME ".out", "r+");
-    check_fopen(file, FILE_BASENAME ".out");
-    while (fgets(energy_string, 60, file) != NULL)
-        sscanf(energy_string, "%*[^-0-9]%lf", &energy);
-    fclose(file);
+        char *current_line = NULL;
+        size_t buffer_size = 0;
+        ssize_t chars_read = 0;
+        char *energy_str_pos = NULL;
+        double energy = 0.0;
+        bool found = false;
 
-    remove(FILE_BASENAME ".in");
-    remove(FILE_BASENAME ".engrad");
-    remove(FILE_BASENAME ".gbw");
-    remove(FILE_BASENAME ".ges");
-    remove(FILE_BASENAME ".opt");
-    remove(FILE_BASENAME ".prop");
-    remove(FILE_BASENAME ".trj");
-    remove(FILE_BASENAME ".xyz");
-    remove(FILE_BASENAME ".out");
+        while ((chars_read = getline(&current_line, &buffer_size, file)) != -1)
+        {
+            if((energy_str_pos = strstr(current_line, "FINAL SINGLE POINT ENERGY")) != NULL)
+            {
+                if(sscanf(energy_str_pos, "%*[^-0-9]%lf", &energy) != 1)
+                {
+                    fprintf(stderr, "Error while calling orca: Could not read energy\n");
+                    exit(EXIT_FAILURE);
+                }
+                found = true;
+                break;
+            }
+        }
+        fclose(file);
 
-    return energy;
+        // remove temporary files created by orca
+        remove(FILE_BASENAME ".in");
+        remove(FILE_BASENAME ".engrad");
+        remove(FILE_BASENAME ".gbw");
+        remove(FILE_BASENAME ".ges");
+        remove(FILE_BASENAME ".opt");
+        remove(FILE_BASENAME ".prop");
+        remove(FILE_BASENAME ".trj");
+        remove(FILE_BASENAME ".xyz");
+        remove(FILE_BASENAME ".out");
+
+        int status;
+        waitpid(pid, &status, 0);
+        if(status != 0)
+        {
+            fprintf(stderr, "Error: Orca returned with non-zero exit status %d\n", status);
+            exit(EXIT_FAILURE);
+        }
+
+        if(!found)
+        {
+            fprintf(stderr, "Error: Orca did not return an energy value\n");
+            exit(EXIT_FAILURE);
+        }
+
+        return energy;
+    }
 }
