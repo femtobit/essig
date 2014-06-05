4890b6d69e34f3290a68f721332fd26aa4174e44
Damian Hofmann <2hofmann@informatik.uni-hamburg.de>
2014-06-05 12:37:55 +0200

    Neue Macros zu util.h hinzugefügt

--- code/main.c
+++ code/main.c
@@ -10,6 +10,7 @@
 #include <time.h>
 
 #include "essig.h"
+#include "util.h"
 
 static const char *usage = "Usage: %s <input>\n";
 
@@ -20,18 +21,12 @@ static const double default_max_angle = 1; // rad
 static const double default_rotation_translation_ratio = 1;
 static const double default_temperature = 293.2; // K
 
-static void exit_with_usage(char *programm_name)
-{
-  fprintf(stderr, usage, programm_name);
-  exit(EXIT_FAILURE);
-}
-
 int main(int argc, char *argv[])
 {
   // TODO: Read command line options
   if(argc > 1)
   {
-    exit_with_usage(argv[0]);
+    FAIL(usage, argv[0]);
   }
 
   srand48(time(NULL));
