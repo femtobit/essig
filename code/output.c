5fb8f6dbac67b21e86cb50bd5dd29b514aff96e2
Damian Hofmann <2hofmann@informatik.uni-hamburg.de>
2014-06-20 19:57:15 +0200

    Speicherfehler in output.c gefixt

--- code/output.c
+++ code/output.c
@@ -4,48 +4,52 @@
 * Authors:
 *   Niek Andresen <2andrese@inf>
 *   Robert Hartmann <2hartman@inf>
+*   Damian Hofmann <2hofmann@inf>
 */
-
-/* To think about: What is the maximum size of coordinates? (~line 28)*/
-
+#define _GNU_SOURCE // for asprintf
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 #include "essig.h"
+#include "util.h"
+
+static char *safe_strcat(char *dest, const char *src)
+{
+  dest = realloc(dest, strlen(dest) + strlen(src) + sizeof(char));
+  CHECK_ALLOC(dest);
+  strcat(dest, src);
+  return dest;
+}
 
 /**
  * Returns a list of the molecule's atoms in the orca/molden format.
  */
 char *molecule_format_atom_list(const Molecule *mol)
 {
-  char *resultS; /* result string */
-  char *temp;
+  char *result; /* result string */
   size_t i, j, atom_count;
   Atom *atom;
 
   atom_count = mol->atom_count;
-  resultS = malloc((1 + atom_count * (3 * 14 + 1 * 2 + 1 * 1)) * sizeof *resultS);
+  result = malloc((1 + atom_count * (3 * 14 + 1 * 2 + 1 * 1)) * sizeof *result);
   /* 1 end of string and 3 coordinates, 1 element symbol and 1 end of line per atom */
-  resultS[0] = '\0';
-
+  result[0] = '\0';
 
   for(i = 0; i < atom_count; ++i)
   {
     atom = mol->atoms + i;
-    resultS = strcat(resultS, atom->element_symbol);
+    result = safe_strcat(result, atom->element_symbol);
     for(j = 0; j < 3; ++j)
     {
-      temp = malloc(14 * sizeof *temp);
-      /* 11 characters for one coordinate should be enough(?) */
-      assert(temp != NULL);
-      sprintf(temp, "   %*.5f", 11, atom->pos.x[j]);
-      resultS = strcat(resultS, temp);
+      char *temp;
+      asprintf(&temp, "   %*.5f", 11, atom->pos.x[j]);
+      result = safe_strcat(result, temp);
       free(temp);
     }
-    resultS = strcat(resultS, "\n");
+    result = safe_strcat(result, "\n");
   }
-  return resultS;
+  return result;
 }
 
 /**
